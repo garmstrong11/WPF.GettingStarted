ed94007f9c564b06c2ddbc5150a492778cf2f43d
GaryHome <gary.m.armstrong@gmail.com>
2015-01-24 16:01:46 -0600

    Complete the Family and Person classes

--- WPF.GettingStarted/Models/Family.cs
+++ WPF.GettingStarted/Models/Family.cs
@@ -1,5 +1,6 @@
 ﻿namespace WPF.GettingStarted.Models
 {
+  using System.Collections.ObjectModel;
   using Catel.Data;
 
   public class Family : ModelBase
@@ -16,9 +17,23 @@
     /// <summary>
     /// Register the FamilyName property so it is known in the class.
     /// </summary>
-    public static readonly PropertyData FamilyNameProperty = RegisterProperty(
-      "FamilyName",
-      typeof(string),
-      null);
+    public static readonly PropertyData FamilyNameProperty = RegisterProperty("FamilyName", typeof(string));
+
+    /// <summary>
+    /// Gets or sets the list of persons in this family.
+    /// </summary>
+    public ObservableCollection<Person> Persons
+    {
+      get { return GetValue<ObservableCollection<Person>>(PersonsProperty); }
+      set { SetValue(PersonsProperty, value); }
+    }
+
+    /// <summary>
+    /// Register the Persons property so it is known in the class.  
+    /// </summary>
+    public static readonly PropertyData PersonsProperty = RegisterProperty(
+      "Persons",
+      typeof(ObservableCollection<Person>),
+      () => new ObservableCollection<Person>());
   }
 }
--- WPF.GettingStarted/Models/Person.cs
+++ WPF.GettingStarted/Models/Person.cs
@@ -4,6 +4,53 @@
 
   public class Person : ModelBase
   {
-     
+    /// <summary>
+    /// Gets or sets the first name of this person.
+    /// </summary>
+    public string FirstName
+    {
+      get { return GetValue<string>(FirstNameProperty); }
+      set { SetValue(FirstNameProperty, value); }
+    }
+
+    /// <summary>
+    /// Register the FirstName property so it is known in the class.
+    /// </summary>
+    public static readonly PropertyData 
+      FirstNameProperty = RegisterProperty("FirstName", typeof (string));
+
+    /// <summary>
+    /// Gets or sets the property value.
+    /// </summary>
+    public string LastName
+    {
+      get { return GetValue<string>(LastNameProperty); }
+      set { SetValue(LastNameProperty, value); }
+    }
+
+    /// <summary>
+    /// Register the LastName property so it is known in the class.
+    /// </summary>
+    public static readonly PropertyData 
+      LastNameProperty = RegisterProperty("LastName", typeof(string));
+
+    public override string ToString()
+    {
+      var fullName = string.Empty;
+      var firstNameNotEmpty = !string.IsNullOrWhiteSpace(FirstName);
+      var lastNameNotEmpty = !string.IsNullOrWhiteSpace(LastName);
+
+      if (firstNameNotEmpty) {
+        fullName += FirstName;
+      }
+      if (firstNameNotEmpty && lastNameNotEmpty) {
+        fullName += " ";
+      }
+      if (lastNameNotEmpty) {
+        fullName += LastName;
+      }
+
+      return fullName;
+    }
   }
-}
+} 
